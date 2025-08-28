a90b254c400ccee90bc93db12766a609c5cdc722
ojai borale <boraleojai@gmail.com>
2025-08-28 12:23:37 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  "projectId": "pips-app-vnphg",
+  "appId": "1:844945116973:web:21b10427234de1479b0da4",
+  "storageBucket": "pips-app-vnphg.firebasestorage.app",
+  "apiKey": "AIzaSyBx_qH9_2wFEvX5JR9aQFDMDfRyp-cZRuU",
+  "authDomain": "pips-app-vnphg.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "844945116973"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { app };

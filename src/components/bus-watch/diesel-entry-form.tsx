58f86c535f5c34cbb71a186d988bf32428854d6e
ojai borale <boraleojai@gmail.com>
2025-08-28 07:27:38 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.label c

--- src/components/bus-watch/diesel-entry-form.tsx
+++ src/components/bus-watch/diesel-entry-form.tsx
@@ -94,11 +94,11 @@ export default function DieselEntryForm() {
           name="dieselAmount"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Diesel Amount</FormLabel>
+              <FormLabel>Diesel liters</FormLabel>
               <FormControl>
                 <Input type="number" placeholder="e.g., 50" {...field} />
               </FormControl>
-              <FormDescription>Enter the amount of diesel added in liters.</FormDescription>
+              <FormDescription>Enter the amount of diesel added.</FormDescription>
               <FormMessage />
             </FormItem>
           )}

9c2379c9061cb23cb061854745f7d59c3a9d28e5
ojai borale <boraleojai@gmail.com>
2025-08-31 07:46:03 +0000

    module.exports = {   allowedDevOrigins: ['local-origin.dev', '*.local-or

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
   experimental: {
-    allowedDevOrigins: [
-      "http://localhost:3001",
-      "http://localhost:3002",
-      "http://localhost:9003"
-    ]
-  }
+    allowedDevOrigins: ['http://localhost:4000'],
+  },
 };
 
 export default nextConfig;
